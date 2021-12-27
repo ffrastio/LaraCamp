85a3265f36fb6a317545328f14bea551e60ea1ad
ffrastio <48426886+ffrastio@users.noreply.github.com>
2021-12-27 12:59:13 +0700

    Slicing home, success, login, checkout

---
+++ app/Http/Controllers/CheckoutController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CheckoutController extends Controller
+{
+    //
+    public function index(Request $request)
+    {
+        return view('checkout');
+    }
+
+    public function success(Request $request)
+    {
+        return view('success');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CheckoutController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
@@ -17,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::get('/checkout', [CheckoutController::class, 'index'])->name('checkout');
+Route::get('/success-checkout', [CheckoutController::class, 'success'])->name('success-checkout');
