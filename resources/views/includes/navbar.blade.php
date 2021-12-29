598cf5ddf99920fae6a796f9e9005040a4cb5cc7
ffrastio <48426886+ffrastio@users.noreply.github.com>
2021-12-29 07:19:13 +0700

    OAuth Finished

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,26 +1,48 @@
 <nav class="navbar navbar-expand-lg navbar-light">
-        <div class="container">
-            <a class="navbar-brand" href="#">
-                <img src="{{ url('assets/images/logo.png') }}" alt="">
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Program</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Mentor</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Pricing</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Business</a>
-                    </li>
-                </ul>
+    <div class="container">
+        <a class="navbar-brand" href="#">
+            <img src="{{ url('assets/images/logo.png') }}" alt="">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Program</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Mentor</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Pricing</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Business</a>
+                </li>
+            </ul>
+            @auth
+                <div class="d-flex user-logged nav-item dropdown no-arrow">
+                    <a href="#" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+                        Halo, {{ Auth::user()->name }}
+                        <img src="{{ Auth::user()->avatar }}" class="user-photo" alt="">
+                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink" style="right: 0; left: auto">
+                            <li>
+                                <a href="#" class="dropdown-item">My Dashboard</a>
+                            </li>
+                            <li>
+                                <a href="#" class="dropdown-item"
+                                    onclick="event.preventDefault(); document.getElementById('logout-form').submit()">Logout</a>
+                                <form action="{{ route('logout') }}" id="logout-form" style="display: none;"
+                                    method="post">
+                                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                </form>
+                            </li>
+                        </ul>
+                    </a>
+                </div>
+            @else
                 <div class="d-flex">
                     <a href="{{ route('login') }}" class="btn btn-master btn-secondary me-3">
                         Sign In
@@ -29,6 +51,7 @@
                         Sign Up
                     </a>
                 </div>
-            </div>
+            @endauth
         </div>
-    </nav>
+    </div>
+</nav>
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\CheckoutController;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\LoginController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,7 +19,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/checkout', [CheckoutController::class, 'index'])->name('checkout');
 Route::get('/success-checkout', [CheckoutController::class, 'success'])->name('success-checkout');
-Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::get('/login', [UserController::class, 'login'])->name('login');
+
+// Configuration Socialite
+Route::get('sign-in-google', [UserController::class, 'google'])->name('sign.in.google');
+Route::get('auth/google/callback', [UserController::class, 'handleProviderCallBack'])->name('user.google.callback');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
