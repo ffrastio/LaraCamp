7f2c991d8e65ddc5746b31b12e5a8d769ca5772b
ffrastio <48426886+ffrastio@users.noreply.github.com>
2022-01-11 15:47:14 +0700

    init midtrans

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -5,12 +5,24 @@ namespace App\Http\Controllers;
 use App\Http\Requests\User\Checkout\Store;
 use App\Models\Camps;
 use App\Models\Checkout;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Str;
+use Midtrans;
+
 
 class CheckoutController extends Controller
 {
     //
+    public function _construct()
+    {
+        Midtrans\Config::$serverKey = env('MIDTRANS_SERVER_KEY');
+        Midtrans\Config::$isProduction = env('MIDTRANS_IS_PRODUCTION');
+        Midtrans\Config::$isSanitized = env('MIDTRANS_IS_SANITIZED');
+        Midtrans\Config::$is3ds = env('MIDTRANS_IS_3DS');
+    }
+
     public function index(Request $request, Camps $camp)
     {
         if ($camp->isRegistered) {
@@ -39,11 +51,68 @@ class CheckoutController extends Controller
 
         //create checkout
         $checkout = Checkout::create($data);
+        $this->getSnapRedirect($checkout);
         return redirect()->route('checkout.success');
     }
 
     public function success(Request $request)
     {
         return view('success');
+    }
+
+    public function getSnapRedirect(Checkout $checkout)
+    {
+        $orderId = $checkout->id . '-' . Str::random(12);
+        $price = $checkout->Camp->price * 1000;
+        $checkout->midtrans_booking_code = $orderId;
+        $transaction_details = [
+            'order_id' => $orderId,
+            'gross_amount' => $price,
+        ];
+
+        $item_details[] = [
+            'id' => $orderId,
+            'price' => $price,
+            'quantity' => 1,
+            'name' => "Payment for {$checkout->Camp->title} camp"
+        ];
+
+        $userData = [
+            "first_name" => $checkout->User->name,
+            "last_name" => "",
+            "email" => $checkout->User->email,
+            "phone" => $checkout->User->phone,
+            "address" => $checkout->User->address,
+            "city" => "",
+            "postal_code" => "",
+            "country_code" => "IDN"
+        ];
+
+        $customer_details = [
+            "first_name" => $checkout->User->name,
+            "last_name" => "",
+            "email" => $checkout->User->email,
+            "phone" => $checkout->User->phone,
+            "billing_address" => $userData,
+            "shipping_address" => $userData
+        ];
+
+        $midtrans_params = [
+            'transaction_details' => $transaction_details,
+            'item_details' => $item_details,
+            'customer_details' => $customer_details
+        ];
+
+        try {
+            //Get Snap payment URL
+            $paymentURL = \Midtrans\Snap::createTransaction($midtrans_params)->redirect_url;
+            $checkout->midtrans_url = $paymentURL;
+            $checkout->save();
+
+            return $paymentURL;
+        } catch (Exception $el) {
+            //throw $th;
+            return false;
+        }
     }
 }
--- app/Models/Checkout.php
+++ app/Models/Checkout.php
@@ -11,7 +11,7 @@ class Checkout extends Model
     use HasFactory, SoftDeletes;
 
     protected $fillable = [
-        'users_id', 'camps_id', 'card_number', 'cvc', 'expired', 'is_paid'
+        'users_id', 'camps_id', 'payment_status', 'midtrans_url', 'midtrans_booking_code'
     ];
 
     public function setExpiredAttribute($value)
